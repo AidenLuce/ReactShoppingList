95ff57655b7e88f33e31236b7728b4596f86fc5e
Aiden Luce <luceaiden4124@gmail.com>
2023-06-06 13:20:11 -0500

    restrict empty item

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,26 +1,29 @@
 const Input = () => {
 
 
-    // updates and logs value after every change
-    const handleFormInput = (e) => {
 
-        console.log(e.target.value)
-    }
     const handleSubmit =(e)=>{
         // Handles data ourselves
         e.preventDefault()
-        // Sanity check
-        console.log('submitted ' + "\"" + e.target.item.value + "\"")
 
-        // Clears the searchbar
-        e.target.item.value = ''
+        let enteredItem = e.target.item.value
+        // Checks to see if empty
+        if (enteredItem === ''){
+            alert("Invalid item")
+        }else{
+            // Sanity check
+            console.log('submitted ' + "\"" + enteredItem + "\"")
+
+            // Clears the searchbar
+            e.target.item.value = ''
+        }
     }
 
     return (
         <>
             <div className='nav-buffer'></div>
                 <form className='input-container' onSubmit={handleSubmit}>
-                    <input type='text' name='item' onChange={handleFormInput} placeholder="Bananas"></input>
+                    <input type='text' name='item'  placeholder="Bananas"></input>
                     <button type="submit">ADD</button>
                 </form>
         </>
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,7 +1,6 @@
 import {item} from "../data.js"
 
 const List = () => {
- item i = new item
 }
 
 export default List;
