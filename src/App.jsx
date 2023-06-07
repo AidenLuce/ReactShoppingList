e7252bd383f8d97ce0fee1560df2f14b98e5ba2a
Aiden Luce <luceaiden4124@gmail.com>
2023-06-07 16:23:25 -0500

    trying to iterate over items array

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import {useState} from "react";
 
-import Form from "./Form.jsx";
-
 import "./App.css"
+import Form from "./Form.jsx";
+import Items from "./Items.jsx";
 
 const App = () =>{
     // Setting Default useState to an empty array
@@ -13,7 +13,6 @@ const App = () =>{
         const newItem = {
             id: items.length+1, //Date.now()
             name: itemName,
-            completed: false,
         }
         // used spread to get all previous values and add newItem to array
         setItems([...items, newItem])
@@ -25,6 +24,8 @@ const App = () =>{
     return <section className="section-center">
         {/* passing addItem down to Form*/}
         <Form addItem={addItem}/>
+        {/* passing items array down to Items*/}
+        <Items items={items}/>
     </section>
 }
 
---
+++ src/SingleItem.jsx
@@ -0,0 +1,8 @@
+const SingleItem = ({itemName}) =>{
+    return (
+        <div className="single-item">
+            <p>{itemName}</p>
+        </div>
+    )
+}
+export default SingleItem;
