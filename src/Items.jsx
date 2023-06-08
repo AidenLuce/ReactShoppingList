e0e224f3be965537e7781071f8ab1b599ce07438
Aiden Luce <luceaiden4124@gmail.com>
2023-06-08 16:11:02 -0500

    Nav Component

--- src/Items.jsx
+++ src/Items.jsx
@@ -2,14 +2,12 @@ import SingleItem from "./SingleItem.jsx";
 
 // grabbing items array  and removeItem function from App.jsx
 const Items = ({items}) =>{
-    // console.log(items)
     return(
     <div className="items">
 
         {/* THIS IS THE PROBLEM*/}
         {/* trying to iterate over items array but it messes up the whole application*/}
         {items.map((item)=>{
-            console.log({item})
             return <SingleItem key={item.id}/*iterator*/ item={item}/*Passes Item object*//>
         })}
     </div>
---
+++ src/Nav.jsx
@@ -0,0 +1,13 @@
+import logo from "../src/assets/react.svg"
+
+const Nav = () =>{
+
+    return(
+        <header className='nav'>
+            <img src={logo} alt=''/>
+            <h1>React Shopping list</h1>
+            <img src={logo} alt=''/>
+        </header>
+    )
+}
+export default Nav
